9a1849e9542ca6977fca73003658bf179eb1c4af
jdbuysse <jdbuysse@gmail.com>
2020-06-03 15:48:19 -0600

    part of it, sorta

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,35 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function fetchTrainers() {
+    return fetch(TRAINERS_URL)
+        .then(resp => resp.json())
+        .then(trainerObjects => renderTrainers(trainerObjects))
+}
+
+
+function renderTrainers(trainers) {
+    trainers.forEach(trainer => addTrainerToPage(trainer));
+}
+
+function addTrainerToPage(trainer) {
+    // using innerHTML doesn't feel like the right solution
+    // should I be using the DOM to appendChild? 
+    const mainSection = document.querySelector('main'); 
+    mainSection.innerHTML += `
+    <div class="card" data-id=${trainer.id}>
+        <p>${trainer.name}</p>
+        <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+        <ul>
+            <li>Placeholder Poke</li>
+        </ul>
+    </div>
+    `
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchTrainers()
+  })
